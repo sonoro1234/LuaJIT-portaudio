fa1b929669a8e868fe3d25411f2de14721e8cc6a
sonoro1234 <sonoroster@gmail.com>
2025-01-29 12:20:05 +0100

    test_sine_devices: allow zero latency and bufferFrames, better Log

--- examples/test_sine_devices.lua
+++ examples/test_sine_devices.lua
@@ -2,11 +2,37 @@ local igwin = require"imgui.window"
 --local win = igwin:SDL(800,400, "audio sine")
 local win = igwin:GLFW(800,400, "audio sine",{vsync=2})
 local ig = win.ig
-local igLOG = ig.Log()
-local function printLOG(...)
-	igLOG:Add(table.concat({...},", ").."\n")
-end
 local ffi = require"ffi"
+----------Log
+local function Log()
+	local L = {}
+	local Buf = ffi.new"ImGuiTextBuffer"
+	local ScrollToBottom = false
+	function L:Add(fmt,args)
+		Buf:appendfv(fmt, args);
+		ScrollToBottom = true
+	end
+	function L:Draw(title)
+		if ig.Button("Clear") then Buf:clear() end
+		ig.BeginChild("scrolling", ig.ImVec2(0,0), false, ig.ImGuiWindowFlags_HorizontalScrollbar);
+		ig.TextUnformatted(Buf:begin());
+		if (ScrollToBottom) then ig.SetScrollHereY(1.0); ScrollToBottom = false end
+		ig.EndChild();
+	end
+	return L
+end
+local igLOG = Log()
+local function strconcat(...)
+	local str=""
+	for i=1, select('#', ...) do
+		str = str .. tostring(select(i, ...)) .. ",\t"
+	end
+	str = str .. "\n"
+	return str
+end
+local function printLOG(...)
+	igLOG:Add(strconcat(...))
+end
 local Pa = require"portaudio_ffi"
 local err = Pa.Initialize()
 assert(err==Pa.NoError)
@@ -40,17 +66,16 @@ end
 local thecallback = Pa.MakeAudioCallback(AudioInit,udatacode)
 
 local auinf = Pa.GetAllInfo()
---local ocombos = auinf.out_combos(ig)
---local oAPI,odevice = auinf.first_out()
+
 local dac
 local odevice = 0
 local SRcombo, BScombo
-
+local zerolat = ffi.new("bool[1]",false)
 local function set_odev(dev)
 	sampleHz = tonumber(SRcombo:get_name())
 	local wantedBsiz = tonumber(BScombo:get_name())
     odevice = dev
-    printLOG(odevice, sampleHz, wantedBsiz)
+    printLOG(auinf.DEVS[odevice].name, sampleHz, wantedBsiz)
 	--if dac close it
 	if dac then 
 		dac:StopStream(); 
@@ -59,31 +84,20 @@ local function set_odev(dev)
 	end
 	if odevice == -1 then return end --bad device
 	local outputParameters = ffi.new("PaStreamParameters",{odevice, 2, Pa.Float32, 0, nil})
-	outputParameters.suggestedLatency = Pa.GetDeviceInfo(outputParameters.device).defaultLowOutputLatency;
-	printLOG(outputParameters.channelCount, "channesl",outputParameters.suggestedLatency)
-	
-	-- local outputParameters = ffi.new"PaStreamParameters"
-	-- outputParameters.device = odevice --Pa.GetDefaultOutputDevice(); --/* default output device */
-    -- if (outputParameters.device == Pa.NoDevice) then
-        -- error("Error: No default output device.\n");
-    -- end
-    -- outputParameters.channelCount = 2       
-    -- outputParameters.sampleFormat = Pa.Float32;-- /* 32 bit floating point output */
-    -- outputParameters.suggestedLatency = Pa.GetDeviceInfo( outputParameters.device ).defaultLowOutputLatency;
-    -- outputParameters.hostApiSpecificStreamInfo = nil;
-	-- print("outdevice",outputParameters.device)
-	-- local err = Pa.IsFormatSupported( nil, outputParameters, sampleHz );
-	-- if( err == Pa.FormatIsSupported ) then print"supported" else print"unsupported" end
+	outputParameters.suggestedLatency = zerolat[0] and 0 or Pa.GetDeviceInfo(outputParameters.device).defaultLowOutputLatency;
+	printLOG( "suggestedLatency",outputParameters.suggestedLatency)
+
+	local err = Pa.IsFormatSupported( nil, outputParameters, sampleHz );
+	if( err == Pa.FormatIsSupported ) then printLOG"supported" else printLOG"unsupported" end
 	
 	local stream, err, errmsg = Pa.OpenStream(nil, outputParameters, sampleHz, wantedBsiz, Pa.ClipOff, thecallback, ud)
-	print(stream, err, errmsg)
 	if not stream then
-		print("ERROR", errmsg)
+		printLOG("ERROR", errmsg)
 		return 
 	end
 	dac = stream
 	local streamInfo = Pa.GetStreamInfo( stream );
-    printLOG( "out latency", streamInfo.outputLatency  );
+    printLOG( "out latency", streamInfo.outputLatency,"SR",  streamInfo.sampleRate, sampleHz*streamInfo.outputLatency);
 	if dac:StartStream()~= Pa.NoError then
 		printLOG("error in start_stream")--,ffi.string(lib.Pa.GetErrorText(err))))
 	end
@@ -95,8 +109,6 @@ local function scandevices()
 	Pa.Initialize()
 	auinf = Pa.GetAllInfo()
 	DevCombo:set( auinf.out_devices.names)
-	-- require"anima"
-	-- prtable(auinf)
 end
 
 local standardSampleRates = {
@@ -108,18 +120,18 @@ SRcombo:set_name("44100")
 
 local bufsizes = {}
 for i= 6,11 do table.insert(bufsizes, tostring(2^i)) end
+table.insert(bufsizes, 1,tostring(0))
 BScombo = ig.LuaCombo("buffer size##in",bufsizes)
 BScombo:set_name("512")
 
 DevCombo = ig.LuaCombo("devices", auinf.out_devices.names, function(val, id)
-	odevice = auinf.out_devices.devID[id] ; printLOG(val,id)
+	odevice = auinf.out_devices.devID[id] ;
 end)
 
 
 local counter = 0
 function win:draw(ig)
 
-	igLOG:Draw("log window")
 	if ig.Button"scan devices" then
 		scandevices()
 	end
@@ -129,9 +141,15 @@ function win:draw(ig)
 	
 	SRcombo:draw()
 	BScombo:draw()
+	
+	ig.Checkbox("zero suggested latency",zerolat)
+	ig.SameLine()
 	if ig.Button"reset device" then
 		set_odev(odevice)
 	end
+	
+	ig.Separator()
+	igLOG:Draw("log window")
 	
     if counter==10 then
 		setFreq(math.random()*500 + 100)
